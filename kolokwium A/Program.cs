1d56d5e546be34b5963d2b63b487a431049b0456
Paulina Szczepańska <paulina.szczepanska122@gmail.com>
2018-01-13 12:00:42 +0100

    zadanie 1

--- kolokwium A/Program.cs
+++ kolokwium A/Program.cs
@@ -6,6 +6,40 @@ using System.Threading.Tasks;
 
 namespace kolokwium_A
 {
+#region zadanie 1
+    class Samochod
+    {
+        private float pojemnoscSilnika;
+        private string nrVIN;
+        public int rocznik;
+        public string nazwa;
+        public float srednieSpalanie;
+        private enum TypTrasy
+        {
+            autostrada = 1
+        };
+
+        private Samochod() { }
+        public static Samochod Create(float pojSil, string nrVIN, int rok, string name, float spalanie)
+        {
+            Samochod auto = new Samochod();
+
+            auto.pojemnoscSilnika = pojSil;
+            auto.nrVIN = nrVIN;
+            auto.rocznik = rok;
+            auto.nazwa = name;
+            auto.srednieSpalanie = spalanie;
+
+            return auto;
+        }
+        public double ObliczSpalanie(double dlugoscTrasy)
+        {
+            return srednieSpalanie * dlugoscTrasy * (double)TypTrasy.autostrada;
+        }
+    }
+#endregion
+
+
     class Program
     {
         static void Main(string[] args)
